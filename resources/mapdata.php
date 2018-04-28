d54ed2d5015f28be087c33333ef861a36dbcb117
K.M.Fahim <frihim@atilimited.net>
2018-04-28 14:04:56 +0600

    Merged

--- resources/mapdata.php
+++ resources/mapdata.php
@@ -8,22 +8,30 @@
  */
 
 # Connect to MySQL database
-$conn = new PDO('mysql:host=192.168.0.201;dbname=faobd_db','maruf','maruf');
+$conn = new PDO('mysql:host=192.168.0.201;dbname=faobd_db_v2','maruf','maruf');
 
 # Build SQL SELECT statement including x and y columns
-$sql = "SELECT * FROM (SELECT x.latitude y, y.longitude x, y.total_species, x.species_desc,x.speciesId,x.fao_biome,x.output
-  FROM (SELECT   d.latitude, group_concat(d.species_desc) species_desc,d.fao_biome,d.speciesId,d.output
-            FROM (SELECT   b.latitude,b.species speciesId,b.fao_biome,b.output,
-                           CONCAT (a.species , ' (' , count(b.species)
-                           , ') ') species_desc
-                      FROM species a, ae b
-                     WHERE a.id_species = b.species and b.latitude>0
-                  GROUP BY a.species, b.latitude) d
-        GROUP BY d.latitude) x,
-       (SELECT   c.latitude, c.longitude, count(c.species) total_species
-            FROM ae c
-        GROUP BY c.latitude, c.longitude) y
- WHERE x.latitude = y.latitude) m";
+// $sql = "SELECT * FROM (SELECT x.latitude y, y.longitude x, y.total_species, x.species_desc,x.speciesId,x.fao_biome,x.output
+//   FROM (SELECT   d.latitude, group_concat(d.species_desc) species_desc,d.fao_biome,d.speciesId,d.output
+//             FROM (SELECT   b.latitude,b.species speciesId,b.fao_biome,b.output,
+//                            CONCAT (a.species , ' (' , count(b.species)
+//                            , ') ') species_desc
+//                       FROM species a, ae b
+//                      WHERE a.id_species = b.species and b.latitude>0
+//                   GROUP BY a.species, b.latitude) d
+//         GROUP BY d.latitude) x,
+//        (SELECT   c.latitude, c.longitude, count(c.species) total_species
+//             FROM ae c
+//         GROUP BY c.latitude, c.longitude) y
+//  WHERE x.latitude = y.latitude) m";
+$sql = "SELECT a.latDD y,a.longDD x,GROUP_CONCAT(DISTINCT(FAOBiomes)) fao_biome, COUNT(FAOBiomes) total_species,
+fnc_ae_species_data(a.LatDD,a.LongDD) species_desc FROM location a
+LEFT JOIN group_location b ON a.ID_Location=b.location_id
+LEFT JOIN ae c ON b.group_id=c.location_group
+LEFT JOIN species d ON c.Species=d.ID_Species
+LEFT JOIN faobiomes e ON a.ID_FAOBiomes=e.ID_FAOBiomes
+WHERE c.ID_AE IS NOT NULL
+GROUP BY LatDD,LongDD";
 
 /*
 * If bbox variable is set, only return records that are within the bounding box
