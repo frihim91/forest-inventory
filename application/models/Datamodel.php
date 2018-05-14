613aa8bca76a242ae9ed1562fe3bc66e50b9dfa1
Rokibuzzaman <rokibuzzaman@atilimited.net>
2018-05-14 14:21:15 +0600

    species list data show

--- application/models/Datamodel.php
+++ application/models/Datamodel.php
@@ -18,14 +18,25 @@ Class Datamodel extends CI_Model {
                               (SELECT COUNT(*) FROM rd as rd LEFT JOIN species_group sr ON rd.Speciesgroup_ID=sr.Speciesgroup_ID
                               LEFT JOIN species s ON sr.ID_Species=s.ID_Species WHERE sr.ID_Species=$speciesId) TOTAL_RD,
                               (SELECT COUNT(*) FROM wd WHERE ID_species=s.ID_Species) TOTAL_WD,
-                              bd.*,s.Species,sl.local_name,si.image_name,g.Genus,sc.*
+                              bd.*,s.Species,sl.local_name,si.image_name,g.Genus
                               FROM species s
                               LEFT JOIN genus g ON s.ID_Genus=g.ID_Genus
                               LEFT JOIN botanical_description bd ON s.ID_Species=bd.species
                               LEFT JOIN species_localname sl ON bd.botanical_id=sl.botanical_id
                               LEFT JOIN species_image si ON bd.botanical_id=si.botanical_id
-                              LEFT JOIN species_character sc ON si.botanical_id=sc.botanical_id
                               where s.ID_Species=$speciesId limit 1")->row();
+    return $result;
+  }
+
+
+
+
+   public function getAvailableDataSpeciesCharacter($speciesId)
+  {
+    $result=$this->db->query("SELECT sc.* FROM species s
+                              LEFT JOIN botanical_description bd ON s.ID_Species=bd.species
+                              LEFT JOIN species_character sc ON bd.botanical_id=sc.botanical_id
+                              where s.ID_Species=$speciesId")->result();
     return $result;
   }
 
